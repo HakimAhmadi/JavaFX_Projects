e4ba44bac430c78c7675f2b2b34f13f5197b5dfa
hakimahmadi <hakimsshah@gmail.com>
2020-12-23 07:52:07 +0600

    MiniWeb added

---
+++ MiniWeb/src/sample/Controller.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    private TextField TextField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button BButton;
+
+    @FXML
+    ArrayList<String> history = new ArrayList<>();
+
+    @FXML
+    void back(ActionEvent event) {
+        webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load(history.get(history.size()-1));
+    }
+
+    @FXML
+    void search(ActionEvent event) {
+        webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        history.add(TextField.getText());
+        webEngine.load(TextField.getText());
+    }
+
+}
